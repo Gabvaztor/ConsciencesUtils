5c96e859ac73510aa218336c3cda7e4c91381140
Oselito <jesusromanalonso@gmail.com>
2019-09-21 20:17:26 +0200

    Region-based segmentation with scikit-image.
    Another attempt using region-based segmentation.

--- main.py
+++ main.py
@@ -1,23 +1,37 @@
 """
 Core project
 """
-from PIL import Image
+from skimage.exposure import histogram
+from imageio import imread
 import numpy as np
 import imageio
-from skimage.feature import canny
-from scipy import ndimage as ndi
 
-img = Image.open("index_photo.png")
-np_img = np.asarray(img)
-r = np_img[:,:,0] #split array into rgb channels
-g = np_img[:,:,1]
-b = np_img[:,:,2]
+img = imread(uri="index_photo.jpg", as_gray=True) #Open image on greyscale.
+hist, hist_centers = histogram(img) #Calculating the histogram of the grey values.
 
-edges_r = canny(r/255.) #edge detection, only support 2D arrays
-edges_g = canny(g/255.) #that's the reason why we split channels
-edges_b = canny(b/255.)
-rgb = np.dstack((edges_r,edges_g,edges_b)) #join channels agaim
-fill_np_img = ndi.binary_fill_holes(rgb).astype("int") # fill the edges we found before
-#maybe it's necesary to use the function in smaller portions of the array to work
+markers = np.zeros_like(img) #Determine markers of the object and the background.
+markers[img <= 35] = 1
+markers[img > 35] = 2
 
-imageio.imwrite('index_photo_removed_pixels.png', fill_np_img) #guardando el array en una imagen
+#The watershed transform floods an image of elevation starting from markers, in order to determine the catchment basins
+#of these markers. Watershed lines separate these catchment basins, and correspond to the desired segmentation.
+from skimage.filters import sobel #The choice of the elevation map is critical for good segmentation. Here, the
+elevation_map = sobel(img)        #amplitude of the gradient provides a good elevation map.
+from skimage.morphology import watershed
+segmentation = watershed(elevation_map, markers) #We use the Sobel operator for computing the amplitude of the gradient.
+
+#Convert the array to binary to use it as an alpha channel.
+segmentation_r = segmentation.reshape(segmentation.shape[0]*segmentation.shape[1])
+for i in range(segmentation_r.shape[0]):
+    if segmentation_r[i] > 1:
+        segmentation_r[i] = 255
+    else:
+        segmentation_r[i] = 0
+
+alpha = segmentation_r.reshape(832, 1100)
+img_rgb = imread("index_photo.jpg")
+img_rgb_np = np.asarray(img_rgb)
+
+#Adding the alpha channel to the image, where 0 is a transparent pixel.
+no_background = np.dstack((img_rgb_np, alpha)).astype("uint8")
+imageio.imwrite('index_photo4_removed_pixels.png', no_background)
